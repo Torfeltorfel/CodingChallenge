ddf4ad9fe6719290a362547269dba024536ba9fe
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-11 19:01:25 +0100

    💄 Add container & style input

--- src/components/stickyBanner/StickyBanner.js
+++ src/components/stickyBanner/StickyBanner.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './stickyBanner.css';
+import React from "react";
+import "./stickyBanner.css";
+import PlayIcon from "./PlayIcon.js";
 
 export default function StickyBanner() {
   return (
-    <div className="stickycontainer">
-      <h1 className="test">Hey, this is Torben</h1>
-    </div>
+    <>
+      <div className="containerStickyBanner">
+        <h1 className="containerStickyBanner--headline">
+          Receive 3 tax tips to start optimizing your taxes!
+        </h1>
+        <form className="email">
+          <input
+            type="email"
+            placeholder="Your email address"
+            className="email--input"
+          ></input>
+          <button type="submit" className="email--button">
+            <PlayIcon />
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
