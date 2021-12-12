1f941ab5ff2fbf297703408f6a520f82faf42106
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-12 10:19:24 +0100

    💄 Add background color

--- src/components/stickyBanner/StickyBanner.js
+++ src/components/stickyBanner/StickyBanner.js
@@ -5,20 +5,23 @@ import PlayIcon from "./PlayIcon.js";
 export default function StickyBanner() {
   return (
     <>
-      <div className="containerStickyBanner">
-        <h1 className="containerStickyBanner--headline">
-          Receive 3 tax tips to start optimizing your taxes!
-        </h1>
-        <form className="email">
-          <input
-            type="email"
-            placeholder="Your email address"
-            className="email--input"
-          ></input>
-          <button type="submit" className="email--button">
-            <PlayIcon />
-          </button>
-        </form>
+      <div className="bannerSection">
+        <div className="bannerSection-gradient"></div>
+        <div className="bannerSection-content">
+          <h2 className="containerStickyBanner--headline">
+            Receive 3 tax tips to start optimizing your taxes!
+          </h2>
+          <form className="email">
+            <input
+              type="email"
+              placeholder="Your email address"
+              className="email--input"
+            ></input>
+            <button type="submit" className="email--button">
+              <PlayIcon />
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
