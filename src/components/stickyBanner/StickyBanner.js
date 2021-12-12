e00dbb42fe35f3c3239d0e98480133cf4388a65a
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-12 12:59:38 +0100

    💄 Add clouds

--- src/components/stickyBanner/StickyBanner.js
+++ src/components/stickyBanner/StickyBanner.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './stickyBanner.css';
-import PlayIcon from './PlayIcon.js';
+import PlayIcon from './assets/PlayIcon.js';
 import menImg from './assets/menImg.svg';
+import cloudLargeSize from './assets/cloud.svg';
+import cloudMidSize from './assets/cloudMidSize.svg';
+import cloudSmallSize from './assets/cloudSmallSize.svg';
 
 export default function StickyBanner() {
   return (
@@ -9,6 +12,16 @@ export default function StickyBanner() {
       <div className="bannerSection">
         <div className="bannerSection-gradient"></div>
         <div className="bannerSection-content">
+          <img
+            src={cloudSmallSize}
+            alt="cloud_image"
+            className="cloudImgTopLeft"
+          />
+          <img
+            src={cloudMidSize}
+            alt="cloud_image"
+            className="cloudImgTopRight"
+          />
           <h2 className="containerStickyBanner--headline">
             Receive 3 tax tips to start optimizing your taxes!
           </h2>
@@ -23,6 +36,11 @@ export default function StickyBanner() {
             </button>
           </form>
           <img src={menImg} alt="men" className="bannerSection-men" />
+          <img
+            src={cloudLargeSize}
+            alt="cloud_image"
+            className="cloudImg-bottom"
+          />
         </div>
       </div>
     </>
