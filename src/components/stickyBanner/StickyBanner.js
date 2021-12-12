7949105eada4a94045827c053d049cf99675f9aa
Torben Feldmann <torben.feldmann1@gmail.com>
2021-12-12 19:19:35 +0100

    ✨ Add error message

--- src/components/stickyBanner/StickyBanner.js
+++ src/components/stickyBanner/StickyBanner.js
@@ -8,7 +8,11 @@ import cloudMidSize from './assets/cloudMidSize.svg';
 import cloudSmallSize from './assets/cloudSmallSize.svg';
 
 export default function StickyBanner() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = (data) => console.log({ data });
 
   return (
@@ -29,21 +33,30 @@ export default function StickyBanner() {
           <h2 className="containerStickyBanner--headline">
             Receive 3 tax tips to start optimizing your taxes!
           </h2>
-          <form className="email" onSubmit={handleSubmit(onSubmit)}>
-            <input
-              {...register('email', {
-                required: true,
-                pattern:
-                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-              })}
-              type="text"
-              placeholder="Your email address"
-              className="email--input"
-            ></input>
-            <button type="submit" className="email--button">
-              <PlayIcon />
-            </button>
-          </form>
+
+          <div className="email-container">
+            <form className="email" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register('email', {
+                  required: true,
+                  pattern:
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                })}
+                type="text"
+                placeholder="Your email address"
+                className="email--input"
+              />
+              <button type="submit" className="email--button">
+                <PlayIcon />
+              </button>
+            </form>
+            {errors.email ? (
+              <p className="email__errorMessage">Please enter a valid email</p>
+            ) : (
+              ''
+            )}
+          </div>
+
           <img src={menImg} alt="men" className="bannerSection-men" />
           <img
             src={cloudLargeSize}
